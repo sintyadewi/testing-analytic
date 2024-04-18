3f910bf514ecaf89e84512c66e113b86396b03f0
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-18 14:23:33 +0800

    Create order seeder

---
+++ database/seeders/OrderSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class OrderSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Order::factory()
+            ->count(1)
+            ->hasItems(2)
+            ->create();
+    }
+}
