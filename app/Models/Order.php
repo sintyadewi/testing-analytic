30af8219c15d393f12f20b0f08863d05d7a868af
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-18 14:22:04 +0800

    Add casts attribute, define factory, add relationship, add query builder

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,10 +2,14 @@
 
 namespace App\Models;
 
+use App\Enums\OrderStatusEnum;
+use Database\Factories\OrderFactory;
+use Illuminate\Contracts\Database\Query\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Factories\Factory;
 
 class Order extends Model
 {
@@ -22,6 +26,10 @@ class Order extends Model
         'status'
     ];
 
+    protected $casts = [
+        'status' => OrderStatusEnum::class,
+    ];
+
     public static function boot()
     {
         parent::boot();
@@ -31,8 +39,23 @@ class Order extends Model
         });
     }
 
+    protected static function newFactory(): Factory
+    {
+        return OrderFactory::new();
+    }
+
     public function items(): HasMany
     {
         return $this->hasMany(OrderItem::class);
+    }
+
+    public function analytics(): HasMany
+    {
+        return $this->hasMany(Analytic::class);
+    }
+
+    public function whereStatus(OrderStatusEnum $status): Builder
+    {
+        return $this->where('status', $status);
     }
 }
