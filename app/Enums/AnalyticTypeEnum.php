aa0532ad50499866d981ae9c0c082e0e168716a7
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-21 23:44:20 +0800

    Define getTotalAmountType & getTotalSalesType method

--- app/Enums/AnalyticTypeEnum.php
+++ app/Enums/AnalyticTypeEnum.php
@@ -10,4 +10,26 @@ enum AnalyticTypeEnum: string
     case TOTAL_SALES_COMPLETED = 'total_sales_completed';
     case TOTAL_SALES_IN_PROGRESS = 'total_sales_in_progress';
     case TOTAL_SALES_WAITING_PAYMENT = 'total_sales_waiting_payment';
+
+    public static function getTotalAmountType(OrderStatusEnum $status): self
+    {
+        $types = [
+            OrderStatusEnum::COMPLETED => self::TOTAL_AMOUNT_COMPLETED,
+            OrderStatusEnum::IN_PROGRESS => self::TOTAL_AMOUNT_IN_PROGRESS,
+            OrderStatusEnum::WAITING_PAYMENT => self::TOTAL_AMOUNT_WAITING_PAYMENT,
+        ];
+
+        return $types[$status];
+    }
+
+    public static function getTotalSalesType(OrderStatusEnum $status): self
+    {
+        $types = [
+            OrderStatusEnum::COMPLETED => self::TOTAL_SALES_COMPLETED,
+            OrderStatusEnum::IN_PROGRESS => self::TOTAL_SALES_IN_PROGRESS,
+            OrderStatusEnum::WAITING_PAYMENT => self::TOTAL_SALES_WAITING_PAYMENT,
+        ];
+
+        return $types[$status];
+    }
 }
