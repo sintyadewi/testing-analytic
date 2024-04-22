ca9ace28e97613fbd979d2925ab85e29a744807a
Sintya Dewi <ayusintyaadewi@gmail.com>
2024-04-22 23:51:49 +0800

    Remove unused codes

--- app/Enums/OrderStatusEnum.php
+++ app/Enums/OrderStatusEnum.php
@@ -9,23 +9,4 @@ enum OrderStatusEnum: string
     case COMPLETED = 'completed';
     case IN_PROGRESS = 'in_progress';
     case WAITING_PAYMENT = 'waiting_payment';
-
-    // public function getTotalSalesType(): AnalyticTypeEnum
-    // {
-    //     return $this->filterAnalyticType('total_sales');
-    // }
-
-    // public function getTotalAmountType(): AnalyticTypeEnum
-    // {
-    //     return $this->filterAnalyticType('total_amount');
-    // }
-
-    // protected function filterAnalyticType(string $type): AnalyticTypeEnum
-    // {
-    //     $matchedEnum = array_filter(AnalyticTypeEnum::cases(), function ($analyticType) use ($type) {
-    //         return Str::contains($analyticType->value, "{$type}_{$this->value}");
-    //     });
-
-    //     return reset($matchedEnum);
-    // }
 }
